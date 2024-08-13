5c358e5469f04033f80858300b768bc467d7455c
Martin Quiroga <martinq@itcsoluciones.com>
2024-08-13 15:33:52 -0300

    Adding ws

---
+++ src/messages-ws/messages-ws.gateway.ts
@@ -0,0 +1,28 @@
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { MessagesWsService } from './messages-ws.service';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({ cors: true })
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  
+  @WebSocketServer() wss: Server;
+
+  constructor(
+    private readonly messagesWsService: MessagesWsService
+  ) {}
+  
+  handleConnection(client: Socket, ...args: any[]) {
+    // console.log('Cliente conectado: ', client.id);
+    this.messagesWsService.registerClient(client);
+    // console.log({conectados: this.messagesWsService.getConnectedClients()});
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
+  }
+  
+  handleDisconnect(client: Socket) {
+    // console.log('Cliente desconectado: ', client.id);
+    this.messagesWsService.removeClient(client.id);
+    // console.log({conectados: this.messagesWsService.getConnectedClients()});
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
+  }
+
+}
