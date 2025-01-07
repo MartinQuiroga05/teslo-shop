003e8a77436f965783853bacdb458bff830382ff
Martin Quiroga <martinq@itcsoluciones.com>
2025-01-07 18:23:14 -0300

    Adding handle messages from server and clients

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,6 +1,7 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
 import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dtos/new-message.dto';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -13,6 +14,9 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
   
   handleConnection(client: Socket, ...args: any[]) {
     // console.log('Cliente conectado: ', client.id);
+    const token = client.handshake.headers.authorization as string;
+    console.log(token);
+    
     this.messagesWsService.registerClient(client);
     // console.log({conectados: this.messagesWsService.getConnectedClients()});
     this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
@@ -23,6 +27,28 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
     this.messagesWsService.removeClient(client.id);
     // console.log({conectados: this.messagesWsService.getConnectedClients()});
     this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
+  }
+
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient(client: Socket, payload: NewMessageDto) {
+    
+    //! Message from server - Emite únicamente al cliente que envió el mensaje.
+    // client.emit('message-from-server', {
+    //   fullName: payload.id, 
+    //   message: payload.message
+    // });
+    
+    //! Message from server - Emitir a todos menos al cliente que envio el mensaje.
+    // client.broadcast.emit('message-from-server', {
+    //   fullName: payload.id, 
+    //   message: payload.message
+    // });
+
+    //! Message from server - Emitir a todos inclusive el cliente que envió el mensaje.
+    this.wss.emit('message-from-server', {
+      fullName: payload.id, 
+      message: payload.message
+    });
   }
 
 }
